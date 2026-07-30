a6136e5d0dad2e4d1f8aaf03385b78177c334a9b
leandro lencina <128265950+leadministrador@users.noreply.github.com>
2026-07-30 00:45:50 -0300

    Preparar búsqueda de carreras futuras

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 
 from flask import Flask, render_template, request, jsonify
 import requests, re, sqlite3, json, os
-from bs4 import BeautifulSoup
 from urllib.parse import urljoin, quote_plus
 from datetime import datetime
 
